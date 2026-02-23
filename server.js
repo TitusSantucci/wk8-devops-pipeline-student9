73794affad4e76cedc20bc3364796d0de04d0389
TitusSantucci <t.santucci.7767@westcliff.edu>
2026-02-23 18:27:04 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello from Student 9 pipeline!"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, "0.0.0.0", () => console.log(`Running on port ${port}`));
